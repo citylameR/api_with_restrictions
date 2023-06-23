422bff104ed5ddcea602c342be40cb72f097fdc3
citylameR <dvdchernichenko@gmail.com>
2023-06-23 18:35:25 +0300

    CHANGE permissions.py

--- advertisements/permissions.py
+++ advertisements/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and obj.creator == request.user
+
+
+class IsNotOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user != obj.creator
